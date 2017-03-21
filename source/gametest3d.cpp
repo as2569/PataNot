a05e482d5b1ebda2c410db9964ecbca1c2b89134
as2569 <as2569@gmail.com>
2017-03-20 20:30:47 -0400

    using leap motion properly

--- source/gametest3d.cpp
+++ source/gametest3d.cpp
@@ -14,132 +14,134 @@
 GLuint vbo;
 GLuint uvs;
 GLuint tex;
-using namespace Leap;
+//using namespace Leap;
 glm::mat4 matrixMVP;
 
 //TO DO, move all the leap motion stuff to appropriate location
-class SampleListener : public Listener {
-  public:
-    virtual void onInit(const Controller&);
-    virtual void onConnect(const Controller&);
-    virtual void onDisconnect(const Controller&);
-    virtual void onExit(const Controller&);
-    virtual void onFrame(const Controller&);
-    virtual void onFocusGained(const Controller&);
-    virtual void onFocusLost(const Controller&);
-    virtual void onDeviceChange(const Controller&);
-    virtual void onServiceConnect(const Controller&);
-    virtual void onServiceDisconnect(const Controller&);
-    virtual void onServiceChange(const Controller&);
-    virtual void onDeviceFailure(const Controller&);
-    virtual void onLogMessage(const Controller&, MessageSeverity severity, int64_t timestamp, const char* msg);
-};
 
-const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
-const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle", "Distal"};
-
-void SampleListener::onInit(const Controller& controller) {
-  std::cout << "Initialized" << std::endl;
-}
-
-void SampleListener::onConnect(const Controller& controller) {
-  std::cout << "Connected" << std::endl;
-}
-
-void SampleListener::onDisconnect(const Controller& controller) {
-  // Note: not dispatched when running in a debugger.
-  std::cout << "Disconnected" << std::endl;
-}
-
-void SampleListener::onExit(const Controller& controller) {
-  std::cout << "Exited" << std::endl;
-}
-
-void SampleListener::onFrame(const Controller& controller) {
-	//Get the most recent frame and report some basic information
-	const Frame frame = controller.frame();
-
-	HandList hands = frame.hands();
-
-	for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl)\
-	{
-		// Get the first hand	
-		const Hand hand = *hl;
-		std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-
-		// Get the hand's normal vector and direction
-		const Vector normal = hand.palmNormal();
-		const Vector direction = hand.direction();
-    }
-  }
-
-void SampleListener::onFocusGained(const Controller& controller) {
-  std::cout << "Focus Gained" << std::endl;
-}
-
-void SampleListener::onFocusLost(const Controller& controller) {
-  std::cout << "Focus Lost" << std::endl;
-}
-
-void SampleListener::onDeviceChange(const Controller& controller) {
-  std::cout << "Device Changed" << std::endl;
-  const DeviceList devices = controller.devices();
-
-  for (int i = 0; i < devices.count(); ++i) {
-    std::cout << "id: " << devices[i].toString() << std::endl;
-    std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
-    std::cout << "  isSmudged:" << (devices[i].isSmudged() ? "true" : "false") << std::endl;
-    std::cout << "  isLightingBad:" << (devices[i].isLightingBad() ? "true" : "false") << std::endl;
-  }
-}
-
-void SampleListener::onServiceConnect(const Controller& controller) {
-  std::cout << "Service Connected" << std::endl;
-}
-
-void SampleListener::onServiceDisconnect(const Controller& controller) {
-  std::cout << "Service Disconnected" << std::endl;
-}
-
-void SampleListener::onServiceChange(const Controller& controller) {
-  std::cout << "Service Changed" << std::endl;
-}
-
-void SampleListener::onDeviceFailure(const Controller& controller) {
-  std::cout << "Device Error" << std::endl;
-  const Leap::FailedDeviceList devices = controller.failedDevices();
-
-  for (FailedDeviceList::const_iterator dl = devices.begin(); dl != devices.end(); ++dl) {
-    const FailedDevice device = *dl;
-    std::cout << "  PNP ID:" << device.pnpId();
-    std::cout << "    Failure type:" << device.failure();
-  }
-}
-
-void SampleListener::onLogMessage(const Controller&, MessageSeverity s, int64_t t, const char* msg) {
-  switch (s) {
-  case Leap::MESSAGE_CRITICAL:
-    std::cout << "[Critical]";
-    break;
-  case Leap::MESSAGE_WARNING:
-    std::cout << "[Warning]";
-    break;
-  case Leap::MESSAGE_INFORMATION:
-    std::cout << "[Info]";
-    break;
-  case Leap::MESSAGE_UNKNOWN:
-    std::cout << "[Unknown]";
-  }
-  std::cout << "[" << t << "] ";
-  std::cout << msg << std::endl;
-}
+//class SampleListener : public Listener {
+//  public:
+//    virtual void onInit(const Controller&);
+//    virtual void onConnect(const Controller&);
+//    virtual void onDisconnect(const Controller&);
+//    virtual void onExit(const Controller&);
+//    virtual void onFrame(const Controller&);
+//    virtual void onFocusGained(const Controller&);
+//    virtual void onFocusLost(const Controller&);
+//    virtual void onDeviceChange(const Controller&);
+//    virtual void onServiceConnect(const Controller&);
+//    virtual void onServiceDisconnect(const Controller&);
+//    virtual void onServiceChange(const Controller&);
+//    virtual void onDeviceFailure(const Controller&);
+//    virtual void onLogMessage(const Controller&, MessageSeverity severity, int64_t timestamp, const char* msg);
+//};
+//
+//const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
+//const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle", "Distal"};
+//
+//void SampleListener::onInit(const Controller& controller) {
+//  std::cout << "Initialized" << std::endl;
+//}
+//
+//void SampleListener::onConnect(const Controller& controller) {
+//  std::cout << "Connected" << std::endl;
+//}
+//
+//void SampleListener::onDisconnect(const Controller& controller) {
+//  // Note: not dispatched when running in a debugger.
+//  std::cout << "Disconnected" << std::endl;
+//}
+//
+//void SampleListener::onExit(const Controller& controller) {
+//  std::cout << "Exited" << std::endl;
+//}
+//
+//void SampleListener::onFrame(const Controller& controller) {
+//	//Get the most recent frame and report some basic information
+//	const Frame frame = controller.frame();
+//
+//	HandList hands = frame.hands();
+//
+//	for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl)\
+//	{
+//		// Get the first hand	
+//		const Hand hand = *hl;
+//		std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+//
+//		// Get the hand's normal vector and direction
+//		const Vector normal = hand.palmNormal();
+//		const Vector direction = hand.direction();
+//    }
+//  }
+//
+//void SampleListener::onFocusGained(const Controller& controller) {
+//  std::cout << "Focus Gained" << std::endl;
+//}
+//
+//void SampleListener::onFocusLost(const Controller& controller) {
+//  std::cout << "Focus Lost" << std::endl;
+//}
+//
+//void SampleListener::onDeviceChange(const Controller& controller) {
+//  std::cout << "Device Changed" << std::endl;
+//  const DeviceList devices = controller.devices();
+//
+//  for (int i = 0; i < devices.count(); ++i) {
+//    std::cout << "id: " << devices[i].toString() << std::endl;
+//    std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+//    std::cout << "  isSmudged:" << (devices[i].isSmudged() ? "true" : "false") << std::endl;
+//    std::cout << "  isLightingBad:" << (devices[i].isLightingBad() ? "true" : "false") << std::endl;
+//  }
+//}
+//
+//void SampleListener::onServiceConnect(const Controller& controller) {
+//  std::cout << "Service Connected" << std::endl;
+//}
+//
+//void SampleListener::onServiceDisconnect(const Controller& controller) {
+//  std::cout << "Service Disconnected" << std::endl;
+//}
+//
+//void SampleListener::onServiceChange(const Controller& controller) {
+//  std::cout << "Service Changed" << std::endl;
+//}
+//
+//void SampleListener::onDeviceFailure(const Controller& controller) {
+//  std::cout << "Device Error" << std::endl;
+//  const Leap::FailedDeviceList devices = controller.failedDevices();
+//
+//  for (FailedDeviceList::const_iterator dl = devices.begin(); dl != devices.end(); ++dl) {
+//    const FailedDevice device = *dl;
+//    std::cout << "  PNP ID:" << device.pnpId();
+//    std::cout << "    Failure type:" << device.failure();
+//  }
+//}
+//
+//void SampleListener::onLogMessage(const Controller&, MessageSeverity s, int64_t t, const char* msg) {
+//  switch (s) {
+//  case Leap::MESSAGE_CRITICAL:
+//    std::cout << "[Critical]";
+//    break;
+//  case Leap::MESSAGE_WARNING:
+//    std::cout << "[Warning]";
+//    break;
+//  case Leap::MESSAGE_INFORMATION:
+//    std::cout << "[Info]";
+//    break;
+//  case Leap::MESSAGE_UNKNOWN:
+//    std::cout << "[Unknown]";
+//  }
+//  std::cout << "[" << t << "] ";
+//  std::cout << msg << std::endl;
+//}
 
 int main(int argc, char *argv[])
 {	
 	using namespace Leap;
-	SampleListener listener;
+	//SampleListener listener;
+	
 	Controller controller;
-
+	Leap::Listener listener;
 	//glm::mat4 scaleMatrix;
 	//glm::mat4 rotateMatrix;
 	//glm::mat4 translateMatrix;
